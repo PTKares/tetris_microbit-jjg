d3e5dc1969f3b5ae69abb7924596fc84f539da9a
PTKares <126270051+PTKares@users.noreply.github.com>
2024-04-10 20:48:25 +0200

    Actualizar main.ts, main.py

--- main.py
+++ main.py
@@ -1,143 +1,230 @@
-from microbit import *
-from random import choice
+def changeLedState (NewState):
+    global x_position
+    global y_position
+    global tetris
+    global block
+    
+    if NewState is True : #Muestra el bloque actual
+        if x_position > 0 :
+            led.plot_brightness(x_position - 1, y_position, max(block[0][0], tetris[y_position][x_position]))
+        if x_position < 5:
+            led.plot_brightness((x_position - 1) + 1, y_position, max(block[0][1], tetris[y_position][x_position + 1]))
+        if x_position > 0 and y_position < 4:
+            led.plot_brightness(x_position - 1, y_position + 1, max(block[1][0], tetris[y_position + 1][x_position]))
+        if x_position < 5 and y_position < 4:
+            led.plot_brightness((x_position - 1) + 1, y_position + 1, max(block[1][1], tetris[y_position + 1][x_position + 1]))
+    else: #Oculta el bloque actual
+        if x_position > 0 :
+            led.plot_brightness(x_position - 1, y_position, tetris[y_position][x_position])
+        if x_position < 5 :
+            led.plot_brightness((x_position - 1) + 1, y_position, tetris[y_position][x_position + 1])
+        if x_position > 0 and y_position < 4:
+            led.plot_brightness(x_position - 1, y_position + 1, tetris[y_position + 1][x_position])
+        if x_position < 5 and y_position < 4:
+            led.plot_brightness((x_position - 1) + 1, y_position + 1, tetris[y_position + 1][x_position + 1])
+    pass
 
-#Configura la cuadricula del Tetris
-grid=[[1,0,0,0,0,0,1],[1,0,0,0,0,0,1],[1,0,0,0,0,0,1],[1,0,0,0,0,0,1],[1,0,0,0,0,0,1],[1,1,1,1,1,1,1]]
-#Almacena una lista de 4 bloques, cada bloque es una cuadrícula de 2x2
-bricks = [[9,9],[9,0]],[[9,9],[0,9]],[[9,9],[9,9]],[[9,9],[0,0]]
-#Selecciona un bloque al azar y lo coloca en el centro/parte superior del display LED (y=0,x=3)
-brick = choice(bricks)
-x=3
-y=0
-frameCount=0
-
-#Función para devolver un máximo de 2 valores
-def max(a,b):
-    if a>=b:
-        return a
-    else:
-        return b
-        
-#Función para ocultar el bloque de 2x2 en el display LED
-def hideBrick():
-    if x>0:
-        display.set_pixel(x-1,y,grid[y][x])
-    if x<5:
-        display.set_pixel(x+1-1,y,grid[y][x+1])
-    if x>0 and y<4:
-        display.set_pixel(x-1,y+1,grid[y+1][x])
-    if x<5 and y<4:
-        display.set_pixel(x+1-1,y+1,grid[y+1][x+1])
-        
-#Función para mostrar el bloque de 2x2 en el display LED
-def showBrick():
-    if x>0:
-        display.set_pixel(x-1,y,max(brick[0][0],grid[y][x]))
-    if x<5:
-        display.set_pixel(x+1-1,y,max(brick[0][1],grid[y][x+1]))
-    if x>0 and y<4:
-        display.set_pixel(x-1,y+1,max(brick[1][0],grid[y+1][x]))
-    if x<5 and y<4:
-        display.set_pixel(x+1-1,y+1,max(brick[1][1],grid[y+1][x+1]))
-
-#Función para rotar el bloque 2x2
-def rotateBrick():
-    pixel00 = brick[0][0]
-    pixel01 = brick[0][1]
-    pixel10 = brick[1][0]
-    pixel11 = brick[1][1]
+#Aplica la rotación a la posición del bloque actual
+def rotateblock():
+    global x_position
+    global y_position
+    global tetris
+    global block
     #Comprueba si la rotación es posible
-    if not ((grid[y][x]>0 and pixel00>0) or (grid[y+1][x]>0 and pixel10>0) or (grid[y][x+1]>0 and pixel01>0) or (grid[y+1][x+1]>0 and pixel11>0)):
-        hideBrick()
-        brick[0][0] = pixel10
-        brick[1][0] = pixel11
-        brick[1][1] = pixel01
-        brick[0][1] = pixel00
-        showBrick()
+    if not (
+            (tetris[y_position][x_position] > 0 and block[0][0] > 0)
+            or (tetris[y_position + 1][x_position] > 0 and block[1][0] > 0)
+            or (tetris[y_position][x_position + 1] > 0 and block[0][1] > 0)
+            or (tetris[y_position + 1][x_position + 1] > 0 and block[1][1] > 0)
+    ):
+        #Oculta el bloque para aplicar la rotación
+        #hideblock()
+        changeLedState(False)
+        #Aplica la rotación
+        block00 = block[0][0]
+        block[0][0] = block[1][0]
+        block[1][0] = block[1][1]
+        block[1][1] = block[0][1]
+        block[0][1] = block00
+        #showblock()
+        changeLedState(True)
 
-#Función para mover el bloque
-def moveBrick(delta_x,delta_y):
-    global x,y
-    move=False
-    #Comprueba si el movimiento es posible: no choca con otros bloques ni con los bordes del display.
-    if delta_x==-1 and x>0:
-        if not ((grid[y][x-1]>0 and brick[0][0]>0) or (grid[y][x+1-1]>0 and brick[0][1]>0) or (grid[y+1][x-1]>0 and brick[1][0]>0) or (grid[y+1][x+1-1]>0 and brick[1][1]>0)):
-            move=True
-    elif delta_x==1 and x<5:
-        if not ((grid[y][x+1]>0 and brick[0][0]>0) or (grid[y][x+1+1]>0 and brick[0][1]>0) or (grid[y+1][x+1]>0 and brick[1][0]>0) or (grid[y+1][x+1+1]>0 and brick[1][1]>0)):
-            move=True
-    elif delta_y==1 and y<4:
-        if not ((grid[y+1][x]>0 and brick[0][0]>0) or (grid[y+1][x+1]>0 and brick[0][1]>0) or (grid[y+1+1][x]>0 and brick[1][0]>0) or (grid[y+1+1][x+1]>0 and brick[1][1]>0)):
-            move=True
-    #Si el movimiento es posible, actualiza las coordenadas x,y del bloque
+#Mueve el bloque en el eje X (Izquierda y Derecha) y en el eje Y (abajo)
+def moveBlock(x_move, y_move):
+    global x_position, y_position
+    move = False
+    
+    #Verifica que el movimiento es posible
+    if x_move == -1 and x_position > 0:
+        if not (
+                (tetris[y_position][x_position - 1] > 0 and block[0][0] > 0)
+                or (tetris[y_position][x_position + 1 - 1] > 0 and block[0][1] > 0)
+                or (tetris[y_position + 1][x_position - 1] > 0 and block[1][0] > 0)
+                or (tetris[y_position + 1][x_position + 1 - 1] > 0 and block[1][1] > 0)
+        ):
+            move = True
+    elif x_move == 1 and x_position < 5:
+        if not ((tetris[y_position][x_position + 1] > 0 and block[0][0] > 0) or (tetris[y_position][x_position + 1 + 1] > 0 and block[0][1] > 0) or (
+                tetris[y_position + 1][x_position + 1] > 0 and block[1][0] > 0) or (tetris[y_position + 1][x_position + 1 + 1] > 0 and block[1][1] > 0)):
+            move = True
+    elif y_move == 1 and y_position < 4:
+        if not ((tetris[y_position + 1][x_position] > 0 and block[0][0] > 0) or (tetris[y_position + 1][x_position + 1] > 0 and block[0][1] > 0) or (
+                tetris[y_position + 1 + 1][x_position] > 0 and block[1][0] > 0) or (tetris[y_position + 1 + 1][x_position + 1] > 0 and block[1][1] > 0)):
+            move = True
+    
+    #Si el movimiento es posible, se puede actualizar los ejes X e Y del bloque
     if move:
-        hideBrick()
-        x+=delta_x
-        y+=delta_y
-        showBrick()
-        
-    #Devuelve True o False para confirmar si el movimiento se ha realizado
+        changeLedState(False)
+        #hideblock()
+        x_position += x_move
+        y_position += y_move
+        changeLedState(True)
+
+    #Devuelve True o False si el movimiento del bloque es posible
     return move
 
-#A function to check for and remove completed lines
-def checkLines():
+
+def moveblockLeft():
+    x_move = -1
+    y_move = 0
+    moveBlock(x_move, y_move)
+
+
+def moveblockRight():
+    x_move = 1
+    y_move = 0
+    return moveBlock(x_move, y_move)
+
+def moveblockDown():
+    x_move = 0
+    y_move = 1
+    return moveBlock(x_move, y_move)
+
+#Comprueba las lineas si alguna esta completa
+def checkCompletedLines():
+    global x_position
+    global y_position
+    global tetris
+    global block
     global score
-    removeLine=False
-    #Comprueba cada linea de una en una
+    removeLine = False
+
+    #Comprueba cada linea de una en una 
     for i in range(0, 5):
-        #Si se rellenan 5 bloques (9), una línea está completa
-        if (grid[i][1]+grid[i][2]+grid[i][3]+grid[i][4]+grid[i][5])==45:
+        
+        #Si una linea ha sido completada entonces esta deberá de ser eliminada y la puntuación aumentada
+        if (tetris[i][1] + tetris[i][2] + tetris[i][3] + tetris[i][4] + tetris[i][5]) == on_led_value * 5:
             removeLine = True
-            #Incrementa la puntuación (10 puntos por linea)
-            score+=10
-            #Elimina la linea y hace que todas las lineas superiores caigan 1 puesto
-            for j in range(i,0,-1):
-                grid[j] = grid[j-1]
-            grid[0]=[1,0,0,0,0,0,1]
+            
+            #Incrementa la puntuación
+            score += winingScore
+            
+            #Elimina la linea y hace que las superiores caigan
+            for j in range(i, 0, -1):
+                tetris[j] = tetris[j - 1]
+            tetris[0] = [1, 0, 0, 0, 0, 0, 1]
     if removeLine:
         #Actualiza el display LED
         for i in range(0, 5):
             for j in range(0, 5):
-                display.set_pixel(i,j,grid[j][i+1])
+                led.plot_brightness(i, j, tetris[j][i + 1])
     return removeLine
+
+def play_music():
+    music.start_melody(music.built_in_melody(Melodies.DADADADUM))
+
+def on_gesture_shake():
+    control.reset()
+    pass
+
+
+def init_game():
+    #Mensaje de inicio
+    basic.show_number(3)
+    basic.show_number(2)
+    basic.show_number(1)
+    basic.show_string("GO")
+
+    #Eventos --------------------------------------------
+
+    #Evento del boton táctil
+    input.on_logo_event(TouchButtonEvent.PRESSED, rotateblock)
+
+    #Evento de los botones
+    input.on_button_pressed(Button.A, moveblockLeft)
+    input.on_button_pressed(Button.B, moveblockRight)
     
-gameOn=True
-score=0
-showBrick()
+    #Micrófono
+    #play_music()
+    
+    #Giroscopio
+    input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+    # --------------------------------------------
+
+
+
+#Starteo de programa -------------------------------------------->>>>>>>>>>>>
+init_game()
+#Intensidad máxima del LED
+on_led_value = 255
+#Puntuacion que se añade cuando se complete una linea
+winingScore = 10
+#Crea una matriz del tetris
+tetris = [[1, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 1],
+          [1, 0, 0, 0, 0, 0, 1], [1, 1, 1, 1, 1, 1, 1]]
+
+#Crea las posibilidades del generamiento de bloques
+blocks = [[on_led_value, on_led_value], [on_led_value, 0]], [[on_led_value, on_led_value], [0, on_led_value]], [
+    [on_led_value, on_led_value], [on_led_value, on_led_value]], [[on_led_value, on_led_value], [0, 0]]
+
+#Selecciona el bloque aleatoriamente
+block = blocks[randint(0, blocks.length - 1)]
+
+#Posicion inicial del primer bloque
+x_position = 2
+y_position = 0
+coutFrame = 0
+
+
+gameOn = True
+score = 0
+changeLedState(True)
+
 #Bucle del programa principal - itera cada 50ms
-while gameOn:
-    sleep(50)
-    frameCount+=1
-    #Captura el input del usuario
-    if button_a.is_pressed() and button_b.is_pressed():
-        rotateBrick()
-    elif button_a.is_pressed():
-        moveBrick(-1,0)
-    elif button_b.is_pressed():
-        moveBrick(1,0)
-    
-    #Cada 15 frames, se intenta mover el bloque abajo
-    if frameCount==15 and moveBrick(0,1) == False:
-        frameCount=0
-        #El movimiento no ha sido posible, el bloque se mantiene en la misma posición
-        grid[y][x]=max(brick[0][0],grid[y][x])
-        grid[y][x+1]=max(brick[0][1],grid[y][x+1])
-        grid[y+1][x]=max(brick[1][0],grid[y+1][x])
-        grid[y+1][x+1]=max(brick[1][1],grid[y+1][x+1])
-        
-        if checkLines()==False and y==0:
-            #El bloque ha alcanzado el top del display LED - Perdiste
-            gameOn=False
+def on_forever():
+    global gameOn
+    global x_position
+    global y_position
+    global tetris
+    global block
+    global blocks
+
+    basic.pause(750)
+
+    moveResult = moveblockDown()
+    if moveResult == False:
+        coutFrame = 0
+        #Si el movimiento no es posible, el bloque se mantiene en la misma posición
+        tetris[y_position][x_position] = max(block[0][0], tetris[y_position][x_position])
+        tetris[y_position][x_position + 1] = max(block[0][1], tetris[y_position][x_position + 1])
+        tetris[y_position + 1][x_position] = max(block[1][0], tetris[y_position + 1][x_position])
+        tetris[y_position + 1][x_position + 1] = max(block[1][1], tetris[y_position + 1][x_position + 1])
+
+        if checkCompletedLines() == False and y_position == 0:
+            # El bloque ha alcanzado el top del display - Game Over
+            gameOn = False
+            #Fin del juego
+            pause(2000)
+            game.game_over()
+            basic.show_string("Game Over")
+            basic.show_string("Score: ")
+            basic.show_string(str(score))
         else:
             #Selecciona un nuevo bloque aleatoriamente
-            x=3
-            y=0
-            brick = choice(bricks)
-            showBrick()
-    
-    if frameCount==15:
-       frameCount=0
+            x_position = 3
+            y_position = 0
+            # ----------------------------------------------
+            block = blocks[randint(0, blocks.length - 1)]
+            changeLedState(True)
 
-#Fin del juego
-sleep(2000)
-display.scroll("Game Over: Score: " + str(score))
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,1 +1,238 @@
+function changeLedState(NewState: boolean) {
+    
+    
+    
+    
+    if (NewState === true) {
+        // Muestra el bloque actual
+        if (x_position > 0) {
+            led.plotBrightness(x_position - 1, y_position, Math.max(block[0][0], tetris[y_position][x_position]))
+        }
+        
+        if (x_position < 5) {
+            led.plotBrightness(x_position - 1 + 1, y_position, Math.max(block[0][1], tetris[y_position][x_position + 1]))
+        }
+        
+        if (x_position > 0 && y_position < 4) {
+            led.plotBrightness(x_position - 1, y_position + 1, Math.max(block[1][0], tetris[y_position + 1][x_position]))
+        }
+        
+        if (x_position < 5 && y_position < 4) {
+            led.plotBrightness(x_position - 1 + 1, y_position + 1, Math.max(block[1][1], tetris[y_position + 1][x_position + 1]))
+        }
+        
+    } else {
+        // Oculta el bloque actual
+        if (x_position > 0) {
+            led.plotBrightness(x_position - 1, y_position, tetris[y_position][x_position])
+        }
+        
+        if (x_position < 5) {
+            led.plotBrightness(x_position - 1 + 1, y_position, tetris[y_position][x_position + 1])
+        }
+        
+        if (x_position > 0 && y_position < 4) {
+            led.plotBrightness(x_position - 1, y_position + 1, tetris[y_position + 1][x_position])
+        }
+        
+        if (x_position < 5 && y_position < 4) {
+            led.plotBrightness(x_position - 1 + 1, y_position + 1, tetris[y_position + 1][x_position + 1])
+        }
+        
+    }
+    
+    
+}
 
+// Aplica la rotación a la posición del bloque actual
+// Mueve el bloque en el eje X (Izquierda y Derecha) y en el eje Y (abajo)
+function moveBlock(x_move: number, y_move: number): boolean {
+    
+    let move = false
+    // Verifica que el movimiento es posible
+    if (x_move == -1 && x_position > 0) {
+        if (!(tetris[y_position][x_position - 1] > 0 && block[0][0] > 0 || tetris[y_position][x_position + 1 - 1] > 0 && block[0][1] > 0 || tetris[y_position + 1][x_position - 1] > 0 && block[1][0] > 0 || tetris[y_position + 1][x_position + 1 - 1] > 0 && block[1][1] > 0)) {
+            move = true
+        }
+        
+    } else if (x_move == 1 && x_position < 5) {
+        if (!(tetris[y_position][x_position + 1] > 0 && block[0][0] > 0 || tetris[y_position][x_position + 1 + 1] > 0 && block[0][1] > 0 || tetris[y_position + 1][x_position + 1] > 0 && block[1][0] > 0 || tetris[y_position + 1][x_position + 1 + 1] > 0 && block[1][1] > 0)) {
+            move = true
+        }
+        
+    } else if (y_move == 1 && y_position < 4) {
+        if (!(tetris[y_position + 1][x_position] > 0 && block[0][0] > 0 || tetris[y_position + 1][x_position + 1] > 0 && block[0][1] > 0 || tetris[y_position + 1 + 1][x_position] > 0 && block[1][0] > 0 || tetris[y_position + 1 + 1][x_position + 1] > 0 && block[1][1] > 0)) {
+            move = true
+        }
+        
+    }
+    
+    // Si el movimiento es posible, se puede actualizar los ejes X e Y del bloque
+    if (move) {
+        changeLedState(false)
+        // hideblock()
+        x_position += x_move
+        y_position += y_move
+        changeLedState(true)
+    }
+    
+    // Devuelve True o False si el movimiento del bloque es posible
+    return move
+}
+
+function moveblockDown(): boolean {
+    let x_move = 0
+    let y_move = 1
+    return moveBlock(x_move, y_move)
+}
+
+// Comprueba las lineas si alguna esta completa
+function checkCompletedLines(): boolean {
+    let i: number;
+    let j: number;
+    
+    
+    
+    
+    
+    let removeLine = false
+    // Comprueba cada linea de una en una 
+    for (i = 0; i < 5; i++) {
+        // Si una linea ha sido completada entonces esta deberá de ser eliminada y la puntuación aumentada
+        if (tetris[i][1] + tetris[i][2] + tetris[i][3] + tetris[i][4] + tetris[i][5] == on_led_value * 5) {
+            removeLine = true
+            // Incrementa la puntuación
+            score += winingScore
+            // Elimina la linea y hace que las superiores caigan
+            for (j = i; j > 0; j += -1) {
+                tetris[j] = tetris[j - 1]
+            }
+            tetris[0] = [1, 0, 0, 0, 0, 0, 1]
+        }
+        
+    }
+    if (removeLine) {
+        // Actualiza el display LED
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 5; j++) {
+                led.plotBrightness(i, j, tetris[j][i + 1])
+            }
+        }
+    }
+    
+    return removeLine
+}
+
+function play_music() {
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum))
+}
+
+function init_game() {
+    // Mensaje de inicio
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("GO")
+    // Eventos --------------------------------------------
+    // Evento del boton táctil
+    input.onLogoEvent(TouchButtonEvent.Pressed, function rotateblock() {
+        let block00: number;
+        
+        
+        
+        
+        // Comprueba si la rotación es posible
+        if (!(tetris[y_position][x_position] > 0 && block[0][0] > 0 || tetris[y_position + 1][x_position] > 0 && block[1][0] > 0 || tetris[y_position][x_position + 1] > 0 && block[0][1] > 0 || tetris[y_position + 1][x_position + 1] > 0 && block[1][1] > 0)) {
+            // Oculta el bloque para aplicar la rotación
+            // hideblock()
+            changeLedState(false)
+            // Aplica la rotación
+            block00 = block[0][0]
+            block[0][0] = block[1][0]
+            block[1][0] = block[1][1]
+            block[1][1] = block[0][1]
+            block[0][1] = block00
+            // showblock()
+            changeLedState(true)
+        }
+        
+    })
+    // Evento de los botones
+    input.onButtonPressed(Button.A, function moveblockLeft() {
+        let x_move = -1
+        let y_move = 0
+        moveBlock(x_move, y_move)
+    })
+    input.onButtonPressed(Button.B, function moveblockRight(): boolean {
+        let x_move = 1
+        let y_move = 0
+        return moveBlock(x_move, y_move)
+    })
+    // Micrófono
+    // play_music()
+    // Giroscopio
+    input.onGesture(Gesture.Shake, function on_gesture_shake() {
+        control.reset()
+        
+    })
+}
+
+//  --------------------------------------------
+// Starteo de programa -------------------------------------------->>>>>>>>>>>>
+init_game()
+// Intensidad máxima del LED
+let on_led_value = 255
+// Puntuacion que se añade cuando se complete una linea
+let winingScore = 10
+// Crea una matriz del tetris
+let tetris = [[1, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0, 1], [1, 1, 1, 1, 1, 1, 1]]
+// Crea las posibilidades del generamiento de bloques
+let blocks = [[[on_led_value, on_led_value], [on_led_value, 0]], [[on_led_value, on_led_value], [0, on_led_value]], [[on_led_value, on_led_value], [on_led_value, on_led_value]], [[on_led_value, on_led_value], [0, 0]]]
+// Selecciona el bloque aleatoriamente
+let block = blocks[randint(0, blocks.length - 1)]
+// Posicion inicial del primer bloque
+let x_position = 2
+let y_position = 0
+let coutFrame = 0
+let gameOn = true
+let score = 0
+changeLedState(true)
+// Bucle del programa principal - itera cada 50ms
+basic.forever(function on_forever() {
+    let coutFrame: number;
+    
+    
+    
+    
+    
+    
+    basic.pause(750)
+    let moveResult = moveblockDown()
+    if (moveResult == false) {
+        coutFrame = 0
+        // Si el movimiento no es posible, el bloque se mantiene en la misma posición
+        tetris[y_position][x_position] = Math.max(block[0][0], tetris[y_position][x_position])
+        tetris[y_position][x_position + 1] = Math.max(block[0][1], tetris[y_position][x_position + 1])
+        tetris[y_position + 1][x_position] = Math.max(block[1][0], tetris[y_position + 1][x_position])
+        tetris[y_position + 1][x_position + 1] = Math.max(block[1][1], tetris[y_position + 1][x_position + 1])
+        if (checkCompletedLines() == false && y_position == 0) {
+            //  El bloque ha alcanzado el top del display - Game Over
+            gameOn = false
+            // Fin del juego
+            pause(2000)
+            game.gameOver()
+            basic.showString("Game Over")
+            basic.showString("Score: ")
+            basic.showString("" + score)
+        } else {
+            // Selecciona un nuevo bloque aleatoriamente
+            x_position = 3
+            y_position = 0
+            //  ----------------------------------------------
+            block = blocks[randint(0, blocks.length - 1)]
+            changeLedState(true)
+        }
+        
+    }
+    
+})
